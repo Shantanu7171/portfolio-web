6bb7689e6d43a3e1cf8d336d68983668bf7779f2
Your Name <your-email@example.com>
2026-03-17 23:28:53 +0530

    Portfolio initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+ import Contact from './components/Contact'
+ import Projects from "./components/Projects";
+import Aos from "aos";
+import "aos/dist/aos.css"
+
+
+const App = () => {
+ useEffect(() => {
+  Aos.init();
+  
+ }, [])
+ 
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Home />
+        
+        <Skills />
+        {/* <Experience /> */}
+        <Projects />
+        <Contact />
+      </div>
+    </>
+  );
+};
+
+export default App;
